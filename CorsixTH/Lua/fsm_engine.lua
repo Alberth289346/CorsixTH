9e9d09aadafb85707a47dd4f0de5f44ff11a38c6
Alberth <alberth289346@gmail.com>
2015-12-22 08:39:39 +0100

    Adding stuff to fsmengine

--- CorsixTH/Lua/fsm_engine.lua
+++ CorsixTH/Lua/fsm_engine.lua
@@ -24,18 +24,34 @@ class "FsmEngine" (Object)
 ---@type FsmEngine
 local FsmEngine = _G["FsmEngine"]
 
+--! Constructor of the FsmEngine class.
+--!param data_store (table) Data storage of the fsms. nil means create own space.
 function FsmEngine:FsmEngine(data_store)
-  -- Lis of state machines. A state machine is a mapping of state name to state data.
-  -- State data is a mapping of the following key/value pairs:
-  -- - "initial": If true, this state is the state selected at startup.
+  -- Lis of state machines. A state machine is a mapping of location name to location data.
+  -- Location data has the following key/value pairs:
+  -- - "initial": If true, this is the location selected at startup.
   -- - "initial_func": Function called on startup (if available).
-  -- - "events": Mapping of event name to edges for this event.
-  -- of event-name to a list of edges.
+  -- - "events": Mapping of event name to a list of edges for this event.
+  --
+  -- An edge has up to three fields:
+  -- - "condition": If existing, a function that returns a boolean whether the edge can be chosen.
+  --   If the field is missing, the edge can always be taken.
+  -- - "action": If available, a function that performs an action as part of the event processing.
+  --   If the field is missing, nothing is done (for the fsm).
+  -- - "next_loc": Location to jump to after performing the event.
+  --   If the field is missing, the location does not change.
+  --
+  -- When an event occurs, its edges are checked from first to last in the
+  -- list. The first one that can be performed is chosen. If all fsms with the
+  -- event have a chosen edge, the edges are executed.
   self.fsms = {}
   self.data_store = data_store or {}
 
   self.fsm_datas = nil
 end
+
+function FsmEngine:addEdge(fsm_name, loc, event, cond_func, action_func, next_loc)
+  
 
 function FsmEngine:finish(fsms)
   self.fsms = fsms -- Just storing count is enough?
